1d21ae984bed4b517eb3c6e2c8e8dd39d7669e6b
RDROSA27 <odoo@ampd1.odoo.com>
2022-09-21 13:51:49 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name':'Odoo Academy',
+    'summary':"""Acdemy app to manage training
+    .Courses
+    .sessions
+    .attendees
+    """,
+    'author':'Odoo',
+    'website': 'https://www.odoo.com',
+    'category': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'license': 'OPL-1',
+    'demo':[
+        
+        
+    ],
+}
